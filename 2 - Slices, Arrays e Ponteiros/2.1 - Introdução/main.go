8beda592f4af349a3c49046ff504477bf5de24b0
lekxandy <lekxandy@gmail.com>
2026-06-03 19:55:18 -0300

    add main.go with implementation for slices and arrays demonstration

--- 2 - Slices, Arrays e Ponteiros/2.1 - Introdução/main.go
+++ 2 - Slices, Arrays e Ponteiros/2.1 - Introdução/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+
+	var (
+		slice = []int{1, 2, 3, 4, 5}  //tamanho dinâmico
+		array = [5]int{1, 2, 3, 4, 5} //tamanho fixo
+	)
+
+	fmt.Println("Tamanho do array: ", len(array))
+	fmt.Println("Capacidade do array: ", cap(array))
+
+	fmt.Println("Tamanho do slice: ", len(slice))
+	fmt.Println("Capacidade do slice: ", cap(slice))
+
+	slice = append(slice, 6) //adiciona um elemento ao slice
+
+	fmt.Println("Slice após adicionar um elemento: ", slice)
+	fmt.Println("Tamanho do slice após adicionar um elemento: ", len(slice))
+	fmt.Println("Capacidade do slice após adicionar um elemento: ", cap(slice))
+
+	printSlice(slice)
+
+	// append nao funciona com arrays, pois eles tem tamanho fixo
+	// array = append(array, 6) //isso vai dar erro
+	// append só funciona com slices
+
+}
+
+func printSlice(slice []int) {
+	for i, v := range slice {
+		fmt.Printf("Index: %d, Value: %d\n", i, v)
+	}
+}
