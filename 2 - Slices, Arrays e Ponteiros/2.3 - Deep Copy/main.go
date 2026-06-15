3571ec89a34b2761aaeb19cd90df7a36f5cb81f3
lekxandy <lekxandy@gmail.com>
2026-06-15 18:54:23 -0300

    refactor deep copy implementation for clarity and reuse

--- 2 - Slices, Arrays e Ponteiros/2.3 - Deep Copy/main.go
+++ 2 - Slices, Arrays e Ponteiros/2.3 - Deep Copy/main.go
@@ -23,15 +23,19 @@ func main() {
 		{3, 4},
 	}
 
-	matriz4 := make([][]int, len(matriz3))
-
-	for i := range matriz3 {
-		matriz4[i] = make([]int, len(matriz3[i]))
-		copy(matriz4[i], matriz3[i])
-	}
+	matriz4 := deepCopy(matriz3)
 	matriz4[0][0] = 99 // Modificando a matriz4, mas a matriz3 não é modificada
 
 	fmt.Println(matriz3)
 	fmt.Println(matriz4)
 
+}
+
+func deepCopy(matriz [][]int) [][]int {
+	result := make([][]int, len(matriz))
+	for i := range matriz {
+		result[i] = make([]int, len(matriz[i]))
+		copy(result[i], matriz[i])
+	}
+	return result
 }
